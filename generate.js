e0599cfdff0fa68deb2f04c2cd53b7cb8f288f8e
Corey Farrell <git@cfware.com>
2019-06-15 07:30:34 -0400

    feat: regenerate using node.js 12

--- generate.js
+++ generate.js
@@ -1,14 +1,28 @@
 // Slightly modified version of
 // https://github.com/sindresorhus/proto-props/blob/04fad48f995428eff7e57ab088601a548fc20f2e/generate.js
 
+// The published module can be used from node.js 0.10 but the development scripts require node.js 12+.
 'use strict';
-var fs = require('fs');
-var jsTypes = require('js-types');
+const fs = require('fs');
+const jsTypes = require('js-types');
 
-var ret = jsTypes.reduce(function (acc, cur) {
-	acc[cur] = Object.getOwnPropertyNames(global[cur]).sort();
+const noLegacyProps = [
+	'BigInt',
+	'BigInt64Array',
+	'BigUint64Array',
+	'Promise',
+	'SharedArrayBuffer'
+];
 
-	return acc;
-}, {});
+// eslint-disable-next-line no-use-extend-native/no-use-extend-native
+const ret = Object.fromEntries(jsTypes.map(cur => {
+	// Inject arguments / caller properties where they existed in node.js 10.
+	const fromNode10 = noLegacyProps.includes(cur) ? [] : ['arguments', 'caller'];
+
+	return [
+		cur,
+		fromNode10.concat(Object.getOwnPropertyNames(global[cur])).sort()
+	];
+}));
 
 fs.writeFileSync('obj-props.json', JSON.stringify(ret, null, '\t'));
