775885835adfe833b5131c24345155d991ca04e5
Corey Farrell <git@cfware.com>
2019-06-15 07:30:34 -0400

    feat: regenerate using node.js 12

--- test.js
+++ test.js
@@ -2,9 +2,10 @@
 import test from 'ava';
 import objProps from './obj-props';
 
-test(t => {
+test('obj-props', t => {
 	t.true(Object.keys(objProps).length > 0);
 	t.truthy(objProps.Array);
 	t.truthy(objProps.Number);
-	t.true(objProps.Array.indexOf('from') !== -1);
+	t.true(objProps.Array.includes('from'));
+	t.true(objProps.Object.includes('fromEntries'));
 });
